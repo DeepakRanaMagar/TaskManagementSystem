ba7c9986833d5a0578a7e6338fcd4d73ef48f771
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-04-11 09:29:51 +0545

    .

--- board/views.py
+++ board/views.py
@@ -1,21 +1,48 @@
 from django.shortcuts import render
-from rest_framework import viewsets, authentication, permissions
+from rest_framework import viewsets, authentication, permissions,filters
 from .models import Sprint, Task
 from .serializers import SprintSerializer, TaskSerializers, UserSerializers
 from django.contrib.auth import get_user_model
+from django_filters.rest_framework import DjangoFilterBackend
 
 User = get_user_model()
 
-class SprintViewSet(viewsets.ModelViewSet):
+class DefaultsMixin(object):
+    authentication_class = [
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    ]
+    permissions_class = (
+        permissions.IsAuthenticated,
+    )
+    paginate_by =25
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
+
+class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
     queryset = Sprint.objects.order_by('end_date')
     serializer_class = SprintSerializer
+    search_fields = ('name',)
+    ordering_fields = (
+        'ends',
+        'name',
+    )
 
-class TaskViewSet(viewsets.ModelViewSet):
+class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializers
+    search_fields = (
+        ''
+    )
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     lookup_field = User.USERNAME_FIELD
     lookup_url_kwarg = User.USERNAME_FIELD
     queryset = User.objects.order_by(User.USERNAME_FIELD)
     serializer_class = UserSerializers
+
